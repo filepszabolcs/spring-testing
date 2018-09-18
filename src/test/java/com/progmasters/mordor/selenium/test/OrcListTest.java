0428b0be1fdd5e3b6f6487a0c4eab4de5fa07c9f
root <admin@progmasters.net>
2018-09-18 21:04:42 +0200

    Update selenium test

--- src/test/java/com/progmasters/mordor/selenium/test/OrcListTest.java
+++ src/test/java/com/progmasters/mordor/selenium/test/OrcListTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,9 +21,36 @@ public class OrcListTest {
     @Test
     public void demo() {
         WebDriver driver = RunEnvironment.getWebDriver();
-        driver.get("https://www.blazemeter.com/selenium");
-        String homeUrl = driver.findElement(By.cssSelector("div#logo> a#logo_image ")).getAttribute("href");
-        assertEquals(homeUrl, "https://www.blazemeter.com/");
+        driver.get("http://localhost:3000/orcList");
+
+        driver.findElement(By.cssSelector("button.navbar-toggler")).click();
+        driver.findElement(By.cssSelector("a[href='/orcForm']")).click();
+        String title = driver.findElement(By.cssSelector("div.jumbotron h3")).getAttribute("innerHTML");
+        assertEquals(title, "Orc form");
+
+        driver.findElement(By.cssSelector("input[name='name']")).sendKeys("Dwarf slayer");
+        driver.findElement(By.cssSelector("input[name='killCount']")).sendKeys("10");
+        new Select(driver.findElement(By.cssSelector("select[name='raceType']"))).selectByVisibleText("Uruk");
+        driver.findElement(By.cssSelector("input[name='weapons'][value='BOW'")).click();
+        driver.findElement(By.cssSelector("input[name='weapons'][value='SHIELD'")).click();
+
+        //WebDriverWait wait = new WebDriverWait(driver, 30);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String listPageTitle = driver.findElement(By.cssSelector("div.jumbotron h3")).getAttribute("innerHTML");
+        assertEquals(listPageTitle, "Orc list");
     }
 
     @After
